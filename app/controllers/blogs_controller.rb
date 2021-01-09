9e847b0938793058bbc6c6791dd78671fefbd379
raw7330 <ryosuke.bdc@gmail.com>
2021-01-09 04:03:10 +0000

    finish 5-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,6 +5,22 @@ class BlogsController < ApplicationController
   def show
   end
 
-  def newedit
+  def new
+    @blog = Blog.new
   end
+
+  def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
+  end
+
+  def edit
+  end
+  
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+  end
+
 end
